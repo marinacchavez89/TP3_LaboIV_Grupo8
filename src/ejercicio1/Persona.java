1aa7a3c71f73947d8baeb8267bc38012c753c26b
Martin Lozano <martin.lozano995@gmail.com>
2025-04-11 15:13:13 -0300

    Creacion de la clase Persona

--- src/ejercicio1/Persona.java
+++ src/ejercicio1/Persona.java
@@ -2,4 +2,43 @@ package ejercicio1;
 
 public class Persona {
 
+	private String nombre;
+	private String apellido;
+	private int dni;
+	
+		
+//contructor
+	public Persona(String nombre, String apellido, int dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+	// getters and setters
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + "]";
+	}
+	
+	
+	
 }
