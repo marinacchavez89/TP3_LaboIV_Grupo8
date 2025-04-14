5a6fc6fc7cebf9cd688a0411342e2a4be041083a
juancruzgauna <juancruzgauna97@gmail.com>
2025-04-13 23:47:16 -0300

    Se crea la clase Archivo y con BufferedReader se lee el archivo

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -9,17 +9,30 @@ public class Principal {
 		System.out.println("¡Proyecto corriendo!");
 		
 
-			try {
-				String dni = JOptionPane.showInputDialog("Ingrese un numero");
-				Verificar.verificarDniInvalido(dni);
-			} catch (DniInvalido e) {
-				System.out.println("Error. El dni deberia ser numerico");
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-
+//			try {
+//				String dni = JOptionPane.showInputDialog("Ingrese un numero");
+//				Verificar.verificarDniInvalido(dni);
+//			} catch (DniInvalido e) {
+//				System.out.println("Error. El dni deberia ser numerico");
+//			}
+//			catch(Exception e)
+//			{
+//				e.printStackTrace();
+//			}
+		
+		Archivo archivo = new Archivo();
+		archivo.setRuta("Personas.txt");
+		
+		if(archivo.existe()) {
+			System.out.println("Existe archivo");
+		}
+		else {
+			System.out.println("No existe el archivo entonces lo creo");
+			//archivo.creaArchivo();
+		}
+		
+		System.out.println("Se lee el archivo Personas.txt");
+		archivo.lee_lineas();
 	}
 
 }
